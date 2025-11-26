85936ad3aeddedbcce5a2f3fdc9158dc709c2030
Kavipriya <kavipriyapalanivel26@gmail.com>
2025-11-26 05:40:44 +0530

    Update settings.py

--- mypro/settings.py
+++ mypro/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIFILES_DIRS=[BASE_DIR/'static']
+STATIFILES_DIRS=[BASE_DIR/'myapp'/'static']
 STATIC_ROOT= BASE_DIR/"staticfiles"
 
 MEDIA_URL= '/media/'
@@ -131,6 +131,7 @@ MEDIA_ROOT=BASE_DIR/'media'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 
 
 
